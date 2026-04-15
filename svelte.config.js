d61d3bd80194c500e2c0432f96fcfc15727f4ced
Daniel Gorra <77399153+pandapog123@users.noreply.github.com>
2026-04-14 22:39:56 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
     }),
     prerender: {
       // prerender all discoverable routes
-      entries: ["/", "/about", "/contact"],
+      entries: ["/", "/about", "/contact", "/achievements"],
     },
   },
 };
